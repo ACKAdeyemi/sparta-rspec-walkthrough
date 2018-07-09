da7d57ca16eedad19c55e7c662374b7793cbac22
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-07-09 15:06:31 +0100

    Added configure function to spec_helper to add 'documentation'(description provided with it) when compiled + 4 tests for calculator calc_engine_spec

---
+++ spec/calc_engine_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'spec_helper'
+
+describe 'basic calc_engine functions work' do
+
+  it 'should correctly add two numbers' do
+  
+  end
+
+  it 'should correctly subtract two numbers' do
+
+  end
+
+  it 'should correctly multiply two numbers' do
+
+  end
+
+  it 'should correctly divide two numbers' do
+
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/calc_engine.rb'
+
+# goes into rspec gem, accesses configure method
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
